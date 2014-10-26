1343d714cdc6d2e2d969585a173215714cbd8b8c
Dax <d.dax@email.com>
2014-10-26 22:30:36 +0100

    Add authenticate_user use_case

--- lib/users/use_cases.rb
+++ lib/users/use_cases.rb
@@ -1,6 +1,6 @@
 require 'users/support/validation_helpers'
 require 'users/use_cases/use_case'
-require 'users/use_cases/authenticate'
+require 'users/use_cases/authenticate_user'
 require 'users/use_cases/delete_user'
 require 'users/use_cases/get_user'
 require 'users/use_cases/get_users'
---
+++ lib/users/use_cases/authenticate_user.rb
@@ -0,0 +1,61 @@
+module Users
+  module UseCases
+    class AuthenticateUser < UseCase
+
+      Result = Bound.required(
+        :error,
+        :user_uid
+      )
+
+      def initialize(input)
+        ensure_valid_input!(input)
+
+        @nickname = input[:nickname]
+        @auth_key = input[:auth_key]
+      end
+
+      def call
+        authenticate_user
+
+        Result.new(:error => error, :user_uid => user_uid)
+      end
+
+      private
+
+      def authenticate_user
+        @authenticator ||= authenticator.for(nickname, auth_key)
+      end
+
+      def user_uid
+        return nil if error
+      end
+
+      def error
+        return nil if @authenticator.kind_of?(Integer)
+      end
+
+      def nickname
+        @nickname
+      end
+
+      def auth_key
+        @auth_key
+      end
+
+      def authenticator
+        Services::Authenticator.new
+      end
+
+      def ensure_valid_input!(input)
+        input.each_pair do |key, value|
+          reason = "The given #{key} was blank or missing"
+
+          unless value.kind_of?(String) && !value.empty?
+            raise_argument_error(reason, value)
+          end
+        end
+      end
+
+    end
+  end
+end
